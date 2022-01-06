2f54e53059b04957d0142a196c8144b47bc8f998
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-01-06 17:59:06 -0500

    Add Slurm task for model eval

--- scripts/slurm/slurm_evaluate_model.sh
+++ scripts/slurm/slurm_evaluate_model.sh
@@ -4,17 +4,22 @@
 #SBATCH --mem=64G
 #SBATCH --ntasks=1
 #SBATCH --job-name=uz32
-#SBATCH --output=/scratch0/jonnesaleva/en_sp32k_uz_sp32k_til.out
 #SBATCH --account=guest
 #SBATCH --partition=guest-gpu
 #SBATCH --qos=low-gpu
 #SBATCH --export=ALL
 
+set -euo pipefail
+
 # Script that bundles together experiment creation, training and evaluation 
 
 # Parse arguments from environment 
+src_lang="${MRL_NMT_SRC_LANG}"
+tgt_lang="${MRL_NMT_TGT_LANG}"
 experiment_name="${MRL_NMT_EXPERIMENT_NAME}"
 model_name="${MRL_NMT_MODEL_NAME}"
+eval_name="${MRL_NMT_EVAL_NAME}"
+eval_model_checkpoint="${MRL_NMT_EVAL_MODEL_CHECKPOINT}"
 references_file="${MRL_NMT_REFERENCES_FILE}"
 raw_data_folder="${MRL_NMT_RAW_DATA_FOLDER}"
 bin_data_folder="${MRL_NMT_BIN_DATA_FOLDER}"
@@ -22,9 +27,46 @@ conda_env_name="${MRL_NMT_ENV_NAME}"
 experiments_prefix="${MRL_NMT_EXPERIMENTS_FOLDER}"
 checkpoints_prefix="${MRL_NMT_CHECKPOINTS_FOLDER}"
 gpu="$CUDA_VISIBLE_DEVICES"
+eval_mode="${MRL_NMT_EVAL_MODE}"
+remove_preprocessing_hypotheses="${MRL_NMT_REMOVE_PREPROCESSING_HYPOTHESES}"
+remove_preprocessing_references="${MRL_NMT_REMOVE_PREPROCESSING_REFERENCES}"
+remove_preprocessing_source="${MRL_NMT_REMOVE_PREPROCESSING_SOURCE}"
+remove_preprocessing_references_clean="${REMOVE_PREPROCESSING_REFERENCES_CLEAN}"
+detokenize_hypotheses="${MRL_NMT_DETOKENIZE_HYPOTHESES}"
+detokenize_references="${MRL_NMT_DETOKENIZE_REFERENCES}"
+detokenize_source="${MRL_NMT_DETOKENIZE_SOURCE}"
+detokenize_references_clean="${MRL_NMT_DETOKENIZE_REFERENCES_CLEAN}"
 
 ### Source necessary function definitions
 source scripts/slurm/slurm_functions.sh
 
+activate_conda_env $conda_env_name
+
 ### Create experiment folder & train/eval folders for default corpus
-prep && train #&& evaluate
+prep_eval \
+    $experiment_name \
+    $model_name \
+    $eval_name \
+    $eval_model_checkpoint \
+    $raw_data_folder \
+    $bin_data_folder \
+    $references_file \
+    $experiments_prefix \
+    $checkpoints_prefix
+
+evaluate \
+    $experiment_name
+    $src_lang
+    $tgt_lang
+    $model_name
+    $references_clean_file
+    $remove_preprocessing_hypotheses
+    $remove_preprocessing_references
+    $remove_preprocessing_source
+    $remove_preprocessing_references_clean
+    $detokenize_hypotheses
+    $detokenize_references
+    $detokenize_source
+    $detokenize_references_clean
+    $gpu_device
+    $eval_mode
--- scripts/slurm/slurm_functions.sh
+++ scripts/slurm/slurm_functions.sh
@@ -4,7 +4,16 @@ activate_conda_env () {
     conda activate $conda_env_name
 }
 
-prep () {
+prep_train () {
+
+    local experiment_name=$1
+    local references_file=$2
+    local raw_data_folder=$3
+    local bin_data_folder=$4
+    local model_name=$5
+    local experiments_prefix=$6
+    local checkpoints_prefix=$7
+
     python scripts/create_experiment.py \
         --experiment-name $experiment_name \
         --references-file $references_file \
@@ -15,36 +24,106 @@ prep () {
         --checkpoints-prefix $checkpoints_prefix
 }
 
+prep_eval () {
+
+    local experiment_name=$1
+    local model_name=$2
+    local eval_name=$3
+    local eval_model_checkpoint=$4
+    local raw_data_folder=$5
+    local bin_data_folder=$6
+    local references_clean_file=$7
+    local experiments_prefix=$8
+    local checkpoints_prefix=$9
+
+    python scripts/create_experiment.py \
+        --experiment-name $experiment_name \
+        --model-name $model_name \
+        --eval-name $eval_name \
+        --eval-model-checkpoint $eval_model_checkpoint \
+        --references-file $references_clean_file \
+        --raw-data-folder $raw_data_folder \
+        --bin-data-folder $bin_data_folder \
+        --experiments-prefix $experiments_prefix \
+        --checkpoints-prefix $checkpoints_prefix
+}
+
 
 train () {
+
+	local experiment_name=$1
+	local model_name=$2
+	local src_lang=$3
+	local tgt_lang=$4
+	local max_tokens=$5
+	local batch_size=$6
+	local max_updates=$7
+	local gpu_device=$8
+	local validate_interval_updates=$9
+	local save_interval_updates=${10}
+	local encoder_embedding_dim=${11}
+	local decoder_embedding_dim=${12}
+	local lr=${13}
+	local encoder_layers=${14}
+	local encoder_attention_heads=${15}
+	local encoder_hidden_size=${16}
+	local decoder_layers=${17}
+	local decoder_attention_heads=${18}
+	local decoder_hidden_size=${19}
+
     guild run nmt:train_transformer -y \
         experiment_name=$experiment_name \
         model_name=$model_name \
-        src_lang=en tgt_lang=uz  \
-        max_tokens=10000 batch_size=96 max_updates=450000  \
-        gpu_device="${gpu}" \
-        validate_interval_updates=10000 \
-        save_interval_updates=50000 \
-        encoder_embedding_dim=512 \
-        decoder_embedding_dim=512 \
-        lr=0.0003 p_dropout=0.2 \
-        encoder_layers=6 encoder_attention_heads=8 encoder_hidden_size=2048 \
-        decoder_layers=6 decoder_attention_heads=8 decoder_hidden_size=2048 
-}
+        src_lang=$src_lang \
+        tgt_lang=$tgt_lang \
+        max_tokens=$max_tokens \
+        batch_size=$batch_size \
+        max_updates=$max_updates \
+        gpu_device=$gpu_device \
+        validate_interval_updates=$validate_interval_updates \
+        save_interval_updates=$save_interval_updates \
+        encoder_embedding_dim=$encoder_embedding_dim \
+        decoder_embedding_dim=$decoder_embedding_dim \
+        lr=$lr \
+        encoder_layers=$encoder_layers \
+        encoder_attention_heads=$encoder_attention_heads \
+        encoder_hidden_size=$encoder_hidden_size \
+        decoder_layers=$decoder_layers \
+        decoder_attention_heads=$decoder_attention_heads \
+        decoder_hidden_size=$decoder_hidden_size
+    }
 
 evaluate () {
+
+	local experiment_name=$1
+	local src_lang=$2
+	local tgt_lang=$3
+	local model_name=$4
+	local references_clean_file=$5
+	local remove_preprocessing_hypotheses=$6
+	local remove_preprocessing_references=$7
+	local remove_preprocessing_source=$8
+	local remove_preprocessing_references_clean=$9
+	local detokenize_hypotheses=${10}
+	local detokenize_references=${11}
+	local detokenize_source=${12}
+	local detokenize_references_clean=${13}
+	local gpu_device=${14}
+    local mode=${15}
+
+
     guild run nmt:evaluate_transformer -y \
         experiment_name=$experiment_name \
-        src_lang=en tgt_lang=uz \
+        src_lang=$src_lang tgt_lang=$tgt_lang \
         model_name=$model_name eval_name="eval_${model_name}" \
         references_clean_file=$references_file \
-        remove_preprocessing_hypotheses=sentencepiece \
-        remove_preprocessing_references=sentencepiece  \
-        remove_preprocessing_source=sentencepiece \
-        remove_preprocessing_references_clean=none \
-        detokenize_hypotheses=no \
-        detokenize_references=no  \
-        detokenize_source=no \
-        detokenize_references_clean=no \
-        gpu_device="${gpu}" mode="test"
+        remove_preprocessing_hypotheses=$remove_preprocessing_hypotheses \
+        remove_preprocessing_references=$remove_preprocessing_references  \
+        remove_preprocessing_source=$remove_preprocessing_source \
+        remove_preprocessing_references_clean=$remove_preprocessing_references_clean \
+        detokenize_hypotheses=$detokenize_hypotheses \
+        detokenize_references=$detokenize_references \
+        detokenize_source=$detokenize_source \
+        detokenize_references_clean=$detokenize_references_clean \
+        gpu_device="${gpu}" mode=${mode}
 }
