c5526aa057cbc3269e3198814de05f92f73a0dd5
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-01-14 14:10:44 -0500

    Normalize based on preliminary results (larger LR better, more updates better)

--- scripts/slurm/en_char_fi_char_newstest2019/_cfg_en_char_fi_char.sh
+++ scripts/slurm/en_char_fi_char_newstest2019/_cfg_en_char_fi_char.sh
@@ -13,12 +13,12 @@ export MRL_NMT_DETOKENIZE_SOURCE=yes
 export MRL_NMT_ENCODER_HIDDEN_SIZE=2048
 export MRL_NMT_ENCODER_LAYERS=6
 export MRL_NMT_ENV_NAME=fairseq-py3.8
-export MRL_NMT_EXPERIMENT_NAME=en_char_fi_char_slurm_test16x_10kupdates
+export MRL_NMT_EXPERIMENT_NAME=en_char_fi_char_slurm
 export MRL_NMT_EXPERIMENTS_FOLDER=/home/jonnesaleva/mrl_nmt22/experiments
-export MRL_NMT_LEARNING_RATE=0.001 # larger LR
+export MRL_NMT_LEARNING_RATE=0.001
 export MRL_NMT_MAX_TOKENS=8192
-export MRL_NMT_MAX_UPDATES=10000
-export MRL_NMT_MODEL_NAME=slurmformer_clipnorm0.1_10k_lr0.001_sqrt
+export MRL_NMT_MAX_UPDATES=20000
+export MRL_NMT_MODEL_NAME=slurmformer_clipnorm0.1_20k_lr0.001 
 export MRL_NMT_P_DROPOUT=0.2
 export MRL_NMT_REMOVE_PREPROCESSING_HYPOTHESES=char
 export MRL_NMT_REMOVE_PREPROCESSING_REFERENCES_CLEAN=none
@@ -27,21 +27,21 @@ export MRL_NMT_REMOVE_PREPROCESSING_SOURCE=char
 export MRL_NMT_SAVE_INTERVAL_UPDATES=50000
 export MRL_NMT_SRC_LANG=en
 export MRL_NMT_TGT_LANG=fi
-export MRL_NMT_VALIDATE_INTERVAL_UPDATES=500 # validate more often
-export MRL_NMT_PATIENCE=3
+export MRL_NMT_VALIDATE_INTERVAL_UPDATES=500 
+export MRL_NMT_PATIENCE=10  # longer patience
 
 # corpus: default-train
 export MRL_NMT_BIN_DATA_FOLDER=/home/jonnesaleva/mrl_nmt22/data-bin/en-fi/en_char_fi_char/default-train/
 export MRL_NMT_EVAL_MODE="dev"
-export MRL_NMT_EVAL_MODEL_CHECKPOINT="/home/jonnesaleva/mrl_nmt22/experiments/en_char_fi_char_slurm_test16x_10kupdates/eval/eval_slurmformer_clipnorm0.1_10k_lr0.001_sqrt/checkpoint"
-export MRL_NMT_EVAL_NAME="eval_slurmformer_clipnorm0.1_10k_lr0.001_sqrt"
+export MRL_NMT_EVAL_MODEL_CHECKPOINT="/home/jonnesaleva/mrl_nmt22/experiments/en_char_fi_char_slurm/train/slurmformer_clipnorm0.1_20k_lr0.001/checkpoints/checkpoint_best.pt"
+export MRL_NMT_EVAL_NAME="eval_slurmformer_clipnorm0.1_20k_lr0.001"
 export MRL_NMT_RAW_DATA_FOLDER=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-fi/en_char_fi_char/default-train
 export MRL_NMT_REFERENCES_FILE=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-fi/en_char_fi_char/default-train/en-fi.dev.detok.fi
 
 # uncomment to evaluate on newstest-2019
 #export MRL_NMT_BIN_DATA_FOLDER=/home/jonnesaleva/mrl_nmt22/data-bin/en-fi/en_char_fi_char/newstest-2019/
 #export MRL_NMT_EVAL_MODE="train"
-#export MRL_NMT_EVAL_MODEL_CHECKPOINT="/home/jonnesaleva/mrl_nmt22/experiments/en_char_fi_char_slurm_test16x/eval/eval_slurmformer_clipnorm0.1_10k_lr0.001_sqrt/checkpoint"
+#export MRL_NMT_EVAL_MODEL_CHECKPOINT="/home/jonnesaleva/mrl_nmt22/experiments/en_char_fi_char_slurm_test16x/eval/eval_slurmformer_clipnorm0.1_20k_lr0.001/checkpoint"
 #export MRL_NMT_EVAL_NAME="newstest-2019"
 #export MRL_NMT_RAW_DATA_FOLDER=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-fi/en_char_fi_char/newstest-2019
 #export MRL_NMT_REFERENCES_FILE=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-fi/en_char_fi_char/newstest-2019/en-fi.train.detok.fi
