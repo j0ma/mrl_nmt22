6c4ea1c8c00b007dd533b42a27cef922c2d1460a
Jonne Saleva <jonne@jonnesaleva.com>
2022-01-31 20:53:44 -0500

    mtpptk cleanup

--- scripts/slurm/hpcc/char/en_char_tr_char/old/_cfg_en_char_tr_char_til.sh
+++
@@ -1,8 +0,0 @@
-export MRL_NMT_EXPERIMENT_NAME=en_char_tr_char_slurm
-export MRL_NMT_MODEL_NAME=slurmformer
-export MRL_NMT_REFERENCES_FILE=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-tr/en_char_tr_char/default-train/en-tr.test.detok.tr
-export MRL_NMT_RAW_DATA_FOLDER=/home/jonnesaleva/datasets/mrl_nmt22/processed/en-tr/en_char_tr_char/default-train
-export MRL_NMT_BIN_DATA_FOLDER=/home/jonnesaleva/mrl_nmt22/data-bin/en-tr/en_char_tr_char/default-train/
-export MRL_NMT_ENV_NAME=fairseq-py3.8
-export MRL_NMT_EXPERIMENTS_FOLDER=/home/jonnesaleva/mrl_nmt22/experiments
-export MRL_NMT_CHECKPOINTS_FOLDER=/home/jonnesaleva/mrl_nmt22/checkpoints
