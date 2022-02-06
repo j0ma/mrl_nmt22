192a3fe20ac6c7dbb068b734c30c3898705c6988
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-02-06 12:52:29 -0500

    Add iconv to convert to UTF-8 if needed

--- scripts/download/download_news_crawl.sh
+++ scripts/download/download_news_crawl.sh
@@ -8,6 +8,8 @@ set -euo pipefail
 
 LANGUAGE=$1
 OUTPUT_FOLDER=$2
+CONV_TO_UTF=${3:-yes}
+DEFAULT_ENC=${4:-ISO-8859-1}
 DEFAULT_FNAME="${LANGUAGE}_mono"
 OUTPUT_FILE="${OUTPUT_FOLDER}/${3:-$DEFAULT_FNAME}"
 
@@ -29,13 +31,23 @@ main () {
     local language=$1
     local output_file=$2
 
+    local iso_to_utf=$3
+    local orig_enc=$4
+
     mkdir -vp $(dirname $output_file)
 
-    get_newscrawl_urls "${language}" | parallel -t wget -O - {} "|" gunzip -c ">>" "${output_file}"
+    #get_newscrawl_urls "${language}" | parallel -t wget -O - {} "|" gunzip -c ">>" "${output_file}"
+
+    if [ "${iso_to_utf}"="yes" ]
+    then
+        local utf8_output_file="${output_file}.utf8"
+        iconv -f "${orig_enc}" -t UTF-8 $output_file > $utf8_output_file
+        rm -v "${output_file}" && mv -v "${utf8_output_file}" "${output_file}"
+    fi
 
     echo "Lines written:"
     wc -l "${output_file}" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
 
 }
 
-main $LANGUAGE $OUTPUT_FILE
+main $LANGUAGE $OUTPUT_FILE $CONV_TO_UTF $DEFAULT_ENC
