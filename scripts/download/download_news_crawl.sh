9b64f32fc8d7b28d9a414acd4b012d2323313e3d
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-02-06 13:18:38 -0500

    Incorporate subsampling and utf8 conversion into download_news_crawl.sh and guild.yml

--- scripts/download/download_news_crawl.sh
+++ scripts/download/download_news_crawl.sh
@@ -8,10 +8,12 @@ set -euo pipefail
 
 LANGUAGE=$1
 OUTPUT_FOLDER=$2
+DEFAULT_FNAME="${LANGUAGE}_mono"
+OUTPUT_FILE="${OUTPUT_FOLDER}/${DEFAULT_FNAME}"
 CONV_TO_UTF=${3:-yes}
 DEFAULT_ENC=${4:-ISO-8859-1}
-DEFAULT_FNAME="${LANGUAGE}_mono"
-OUTPUT_FILE="${OUTPUT_FOLDER}/${3:-$DEFAULT_FNAME}"
+SUBSAMPLE=${5:-yes}
+SUBSAMPLE_SIZE=${6:-5000000}
 
 get_newscrawl_urls () {
 
@@ -25,6 +27,13 @@ get_newscrawl_urls () {
     rg -o "https://data\.statmt\.org/news-crawl.*\.gz\b" \
         < $temp_file | sed "s/\">.*$//g"
 }
+convert_to_utf8 () {
+    local orig_output_file=$1
+    local utf8_output_file=$2
+    local orig_enc=$3
+    iconv -f "${orig_enc}" -t UTF-8 $orig_output_file > $utf8_output_file
+    rm -v "${orig_output_file}" && mv -v "${utf8_output_file}" "${orig_output_file}"
+}
 
 main () {
 
@@ -33,16 +42,25 @@ main () {
 
     local iso_to_utf=$3
     local orig_enc=$4
+    
+    local subsample=$5
+    local subsample_size=$6
 
     mkdir -vp $(dirname $output_file)
 
-    #get_newscrawl_urls "${language}" | parallel -t wget -O - {} "|" gunzip -c ">>" "${output_file}"
+    get_newscrawl_urls "${language}" | parallel -t wget -O - {} "|" gunzip -c ">>" "${output_file}"
+
+    if [ "${subsample}"="yes" ]
+    then
+        local subsampled_output_file="${output_file}.sub"
+        shuf -n ${subsample_size} < $output_file > $subsampled_output_file
+        local output_file=${subsampled_output_file}
+    fi
 
     if [ "${iso_to_utf}"="yes" ]
     then
         local utf8_output_file="${output_file}.utf8"
-        iconv -f "${orig_enc}" -t UTF-8 $output_file > $utf8_output_file
-        rm -v "${output_file}" && mv -v "${utf8_output_file}" "${output_file}"
+        convert_to_utf8 $output_file $utf8_output_file $orig_enc
     fi
 
     echo "Lines written:"
@@ -50,4 +68,10 @@ main () {
 
 }
 
-main $LANGUAGE $OUTPUT_FILE $CONV_TO_UTF $DEFAULT_ENC
+main \
+    $LANGUAGE \
+    $OUTPUT_FILE \
+    $CONV_TO_UTF \
+    $DEFAULT_ENC \
+    $SUBSAMPLE \
+    $SUBSAMPLE_SIZE
